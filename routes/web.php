d563f11aebae762b7a741b936e29bf33ec334c57
蔡羽宣 <3a832055@gm.student.ncut.edu.tw>
2021-12-26 21:43:48 +0800

    作業訂正:路由函數名稱修正

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,6 @@ Route::prefix('admin')->group(function () {
     Route::get('posts/{id}/edit', [AdminPostsController::class, 'edit'])->name('admin.posts.edit');
 
     Route::post('posts', [AdminPostsController::class, 'store'])->name('admin.posts.store');
-    Route::path('posts/{post}',[AdminPostsController::class,'update'])->name('admin.posts.update');
+    Route::patch('posts/{post}',[AdminPostsController::class,'update'])->name('admin.posts.update');
     Route::delete('posts/{post}',[AdminPostsController::class, 'destroy'])->name('admin.posts.destroy');
 });
